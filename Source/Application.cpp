d0cf5701b1deca70a45c0faeea87ced09554a560
sonicreverb <vlad.krasivin@yandex.ru>
2024-07-09 22:28:24 +0300

    Music playback added.

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -1,8 +1,15 @@
 #include "../Headers/Application.h"
+#include <SFML/Audio.hpp>
 
 void Aplication::run()
 {
 	sf::Clock clock;
+	sf::Music music;
+
+	if (music.openFromFile("MusicBG.ogg")) {
+		music.setLoop(true);
+		music.play();
+	}
 
 	while (this->window.isOpen()) {
 		sf::Time deltaTime = clock.restart();
